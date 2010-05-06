f7bbbbcbc229b9ad548df63af9bbb3356833ed58
Julien Bert <bert.jul@gmail.com>
2010-05-06 18:08:07 +0200

    work to LMEM algorithm

--- pet.py
+++ pet.py
@@ -18,11 +18,11 @@
 # FIREwire Copyright (C) 2008 - 2010 Julien Bert 
 
 # simulation of gamma photon to PET with four detectors
-def pet_2D_square_test_sim_LOR(posx, posy, nx, rnd = 10):
+def pet_2D_square_test_sim_LOR(posx, posy, nx):
     from math         import pi, cos, sin
-    from numpy.random import poisson, seed
-    from random       import random
-    seed(rnd)
+    from numpy.random import poisson
+    from random       import random, expovariate
+
     # detector
       # # #
     # o o o #
@@ -31,11 +31,17 @@ def pet_2D_square_test_sim_LOR(posx, posy, nx, rnd = 10):
       # # #
     
     # Poisson distribution
-    if random() >= 0.5: x0 = posx + poisson(lam=1, size=(1))[0]
-    else:               x0 = posx - poisson(lam=1, size=(1))[0]
-    if random() >= 0.5: y0 = posy + poisson(lam=1, size=(1))[0]
-    else:               y0 = posy - poisson(lam=1, size=(1))[0]
-
+    v = poisson(lam=2.0, size=(1))[0]
+    r1 = random()
+    r2 = random()
+    #v = expovariate(1.0)
+    if   r1 >= 0.66: x0 = posx + v
+    elif r1 >= 0.33: x0 = posx - v
+    else:            x0 = posx
+    if   r2 >= 0.66: y0 = posy + v
+    elif r2 >= 0.33: y0 = posy - v
+    else:            y0 = posy
+    
     # uniform angle
     alpha = random() * pi
 
@@ -105,12 +111,14 @@ def pet_2D_square_test_sim_LOR(posx, posy, nx, rnd = 10):
 # create a list-mode from a simple simulate data
 def pet2D_square_test_LOR(nx, posx, posy, nbp, rnd = 10):
     from numpy        import zeros, array
+    from random       import seed
+    seed(10)
 
     crystals = zeros((nx*nx, nx*nx), 'f')
     image    = zeros((nx, nx), 'f')
     
     for p in xrange(nbp):
-        id1, id2, x0, y0 = pet_2D_square_test_sim_LOR(posx, posy, nx, rnd = 10)
+        id1, id2, x0, y0 = pet_2D_square_test_sim_LOR(posx, posy, nx)
         crystals[id2, id1] += 1.0
         image[y0, x0]      += 1.0
 
@@ -132,8 +140,52 @@ def pet2D_square_test_LOR(nx, posx, posy, nbp, rnd = 10):
 
     return LOR_val, LOR_id1, LOR_id2, image
 
-# build the System Response Matrix for 2D pet test
-def pet2D_square_build_SRM(LOR_val, LOR_id1, LOR_id2, nx):
+# build the sensibility matrix based in the system response matrix for all possible LOR
+def pet2D_square_build_SM(nx):
+    from numpy  import zeros, ones
+    from kernel import kernel_build_2D_SRM_BLA
+    from utils  import image_1D_projection
+
+    nlor    = 6 * nx * nx  # pet 4 heads
+    SRM     = zeros((nlor, nx * nx), 'i')
+    line    = zeros((4 * nlor), 'i')
+    LOR_val = ones((nlor), 'i')
+
+    # first head
+    i = 0
+    for x1 in xrange(nx):
+        for y2 in xrange(nx):
+            line[i:i+4] = [x1, 0, nx-1, y2]
+            i += 4
+        for x2 in xrange(nx):
+            line[i:i+4] = [x1, 0, x2, nx-1]
+            i += 4
+        for y2 in xrange(nx):
+            line[i:i+4] = [x1, 0, 0, y2]
+            i += 4
+    # second head
+    for y1 in xrange(nx):
+        for x2 in xrange(nx):
+            line[i:i+4] = [nx-1, y1, x2, nx-1]
+            i += 4
+        for y2 in xrange(nx):
+            line[i:i+4] = [nx-1, y1, 0, y2]
+            i += 4
+    # third head
+    for x1 in xrange(nx):
+        for y2 in xrange(nx):
+            line[i:i+4] = [x1, nx-1, 0, y2]
+            i += 4
+
+    kernel_build_2D_SRM_BLA(SRM, LOR_val, line, nx)
+    norm = image_1D_projection(SRM, 'x')
+    SRM  = SRM.astype('f')
+    for i in xrange(nlor): SRM[i] /= float(norm[i])
+    
+    return image_1D_projection(SRM, 'y')
+
+# build the System Response Matrix for a list of LOR detected
+def pet2D_square_build_SRM_LOR(LOR_val, LOR_id1, LOR_id2, nx):
     from numpy import zeros
     from kernel import kernel_build_2D_SRM_BLA
     
